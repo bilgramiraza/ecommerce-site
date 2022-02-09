97104711c6175e51bdccc769ab4dbd5e79700857
Raza Hassan <bilgramiraza@yahoo.com>
2022-02-09 12:05:34 +0530

    Added Header and Footer Components

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+      <footer>
+          <p>By Syed Mohd Raza Hassan Bilgrami</p>
+          <img src='' alt='github'/>
+      </footer>
+  )
+}
+
+export default Footer;
