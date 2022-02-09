2a895f0a9f5c4e256ff16ad92a056dd4af4ae884
Raza Hassan <bilgramiraza@yahoo.com>
2022-02-09 12:51:10 +0530

    Added div to test sending user to specific custom urls

--- src/pages/Catalogue.js
+++ src/pages/Catalogue.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Catalogue = () => {
+    let navigate = useNavigate();
   return (
-    <div>Catalogue</div>
+    <div>
+        <h2>Catalogue</h2>
+        <div onClick={()=>navigate(`/products/${parseInt(Math.random()*100)}`)}>
+            Product
+        </div>
+    </div>
   )
 }
 
