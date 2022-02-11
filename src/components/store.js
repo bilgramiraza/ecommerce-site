cf59be6dc4fb93afbf7ec1055aaea708b41bfd6a
Raza Hassan <bilgramiraza@yahoo.com>
2022-02-11 12:17:29 +0530

    implemented rudamentary JS file to get Store item details

---
+++ src/components/store.js
@@ -0,0 +1,37 @@
+function store() {
+    const items=[
+        {
+            id:'1',
+            title:'item1',
+            price:1,
+            img:'',
+        },
+        {
+            id:'2',
+            title:'item2',
+            price:2,
+            img:'',
+        },
+        {
+            id:'3',
+            title:'item3',
+            price:3,
+            img:'',
+        },
+        {
+            id:'4',
+            title:'item4',
+            price:4,
+            img:'',
+        },
+        {
+            id:'5',
+            title:'item5',
+            price:5,
+            img:'',
+        },
+    ];
+    return items;
+}
+
+export default store;
